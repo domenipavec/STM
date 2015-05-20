0cf7846802612b703c12b48cb308d7404087c764
Domen Ipavec <domen@ipavec.net>
2015-05-20 13:29:59 +0200

    Base setup with OpenCV dependency detection and stm package.
    Signed-off-by: Domen Ipavec <domen@ipavec.net>

---
+++ setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------
+# setup.py
+# 
+# setup for STM
+# ----------------------------------------------------------------
+# copyright (c) 2015 - Domen Ipavec
+# Distributed under The MIT License, see LICENSE
+# ----------------------------------------------------------------
+
+from setuptools import setup, find_packages
+
+try:
+    import cv2
+except ImportError:
+    print("OpenCV for Python is required and is not installed.\nPlease install OpenCV and try again.")
+    exit()
+
+setup(
+    name='STM',
+    
+    version='0.1',
+    
+    packages=['stm'],
+    
+    test_suite='nose.collector',
+    
+    install_requires=['nose'],
+    
+    entry_points={
+        'console_scripts': [
+#            'cssqc=cssqc:main',
+        ],
+    },
+    
+    package_data={
+        'cssqc': [ ],
+    }
+)
